ead5b0d4953b8889091577cb583ea525119c80d8
Insurikai <40343703+Insurikai@users.noreply.github.com>
2019-10-29 13:17:13 -0700

    updated usercard and app.js, app is now a class component

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import UserCard from './components/UserCard';
-import axios from 'axios';
+import * as axios from 'axios';
 import './App.css';
 
-function App() {
-
-
-  return (
-    <div className="App">
-      <UserCard userInfo={user}/>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      user: {}
+    }
+    this.componentDidMount = this.componentDidMount.bind(this);
+  }
+  componentDidMount(){
+    axios.get('https://api.github.com/users/Insurikai').then((results)=>{
+      this.setState({
+        user:results.data
+      });
+    }).catch(console.log);
+  }
+  
+  render(){
+    console.log('render');
+    return (
+      <div className="App">
+        <h1> done</h1>
+        <UserCard userInfo={this.state.user}/>
+      </div>
+    );
+  }
 }
-
-export default App;
--- github-user-card/src/components/UserCard.js
+++ github-user-card/src/components/UserCard.js
@@ -1,24 +1,15 @@
 import React from 'react';
 
 export default class UserCard extends React.Component{
-    constructor(){
-        super();
-        this.state = {
-            user: {}
-        }
-        this.componentDidMount = this.componentDidMount.bind(this);
-    }
-
-    componentDidMount(){
-        this.state.user = this.props.userInfo;
-    }
-
     render(){
-
-        return(
-            <>
-            <h1>{this.state.user.login}</h1>
-            </>
-        )
+        if(this.props.userInfo){
+            return(
+                <>
+                <h1>{this.props.userInfo.login}</h1>
+                </>
+            )
+        }else{
+            return (null);
+        }
     }
 }
