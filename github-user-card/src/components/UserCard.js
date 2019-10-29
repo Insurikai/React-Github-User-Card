3764a7da17422a16eb99b62b6b145773ce525aa8
Insurikai <40343703+Insurikai@users.noreply.github.com>
2019-10-29 13:36:31 -0700

    added styling and updated the JSX to work

--- github-user-card/src/components/UserCard.js
+++ github-user-card/src/components/UserCard.js
@@ -3,12 +3,14 @@ import React from 'react';
 export default (props) => {
     return(
         <div className="user-card">
-            <h1 className="name">{props.userInfo.login}</h1>
-            {
-                props.followers.map( (follower)=>{
-                    return(<p className="follower">{follower}</p>)
-                })
-            }
+            <h1 className="name">{/*props.userInfo.login*/ 'Insurikai'}</h1>
+            <div className="follower-container">
+                {
+                    props.followers.map( (follower)=>{
+                        return(<p className="follower">{follower}</p>)
+                    })
+                }
+            </div>
         </div>
     )
 }
